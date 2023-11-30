dc81e1d2b011a67ef47e10d0ad715aaf9ad74d76
Paul Heinrich <pheinrich11235@gmail.com>
2023-11-30 11:19:30 +0100

    add some stuff

--- src/test/java/org/matsim/project/RunMatsimTest.java
+++ src/test/java/org/matsim/project/RunMatsimTest.java
@@ -27,6 +27,7 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
