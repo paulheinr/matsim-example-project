17474f903b93d77649cb00958ccc38b5dc27f495
Paul Heinrich <55242299+paulheinr@users.noreply.github.com>
2026-05-04 18:18:30 +0200

    add analysis file

---
+++ src/main/java/org/matsim/analysis/SimpleEventHandler.java
@@ -0,0 +1,23 @@
+package org.matsim.analysis;
+
+import org.apache.commons.lang3.event.EventUtils;
+import org.matsim.api.core.v01.events.LinkEnterEvent;
+import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
+import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.events.EventsUtils;
+
+public class SimpleEventHandler {
+    static void main() {
+        EventsManager eventsManager = EventsUtils.createEventsManager();
+        MyEventHandler myHandler = new MyEventHandler();
+        eventsManager.addHandler(myHandler);
+        EventsUtils.readEvents(eventsManager, "output/output_events.xml.zst");
+    }
+
+    static class MyEventHandler implements LinkEnterEventHandler {
+        @Override
+        public void handleEvent(LinkEnterEvent event) {
+            System.out.println("Vehicle " + event.getVehicleId() + " entered link " + event.getLinkId() + " at time " + event.getTime());
+        }
+    }
+}
